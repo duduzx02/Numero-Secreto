ab4070393ae73c5fab75aece395dc91cc76c45d5
duduzx02 <duduzx02@gmail.com>
2023-06-10 03:29:30 -0300

    Acertando o número secreto

--- app/validacao.js
+++ app/validacao.js
@@ -3,11 +3,20 @@ function verificaOresultadoValido(chute) {
   const numero = +chute;
 
   if (chuteForInvalido(numero)) {
-    console.log('Valor inválido!')
+    elementoChute.innerHTML = '<div class="invalido">Valor inválido</div>';
   }
 
   if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
-    console.log(`Valor inválido: o número secreto precisa estar entre ${menorValor} e ${maiorValor}`)
+    elementoChute.innerHTML = `<div class="invalido"> Valor inválido: o número secreto precisa estar entre ${menorValor} e ${maiorValor}</div>` 
+  }
+
+  if (numero === numeroSecreto){
+    document.body.innerHTML = `
+      <div class="body">
+      <h1>Você acertou</h1>
+      <h2>O número secreto era ${numeroSecreto}</h2>
+      </div>
+    `
   }
 }
 
